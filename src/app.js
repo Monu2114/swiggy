d96fdbec66063b1c78dc46de5cf4f5a830c7f0b2
Monu_2114 <monu.nanabala@gmail.com>
2025-09-04 18:34:48 +0530

    useRouteError ,react-router-dom episode 7

--- src/app.js
+++ src/app.js
@@ -1,8 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router";
 import Header from "./components/Header";
 import Body from "./components/Body";
-
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Error from "./components/Error";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 const AppLayout = () => {
@@ -13,4 +16,20 @@ const AppLayout = () => {
     </>
   );
 };
-root.render(<AppLayout />);
+
+const appRouter = createBrowserRouter([
+  {
+    path: "/",
+    element: <AppLayout />,
+    errorElement: <Error />,
+  },
+  {
+    path: "/about",
+    element: <About />,
+  },
+  {
+    path: "/contact",
+    element: <Contact />,
+  },
+]);
+root.render(<RouterProvider router={appRouter} />);
---
+++ src/components/Contact.js
@@ -0,0 +1,11 @@
+const Contact = () => {
+  return (
+    <div>
+      <h1>Contact Us</h1>
+      <h2>Location</h2>
+      <h2>Phone Number</h2>
+    </div>
+  );
+};
+
+export default Contact;
