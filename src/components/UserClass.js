fd9e2fb8c10937b3adcdfe1d0e0fff34736b5ff1
Monu_2114 <monu.nanabala@gmail.com>
2025-09-21 19:10:26 +0530

    error

--- src/components/UserClass.js
+++ src/components/UserClass.js
@@ -1,45 +1,45 @@
-import { userInfo } from "os";
-import React from "react";
+// import { userInfo } from "os";
+// import React from "react";
 
-export default class UserClass extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      userInfo: {
-        name: "Dummy",
-        location: "Default",
-        avatar_url: "bla bla",
-      },
-    };
-  }
+// export default class UserClass extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       userInfo: {
+//         name: "Dummy",
+//         location: "Default",
+//         avatar_url: "bla bla",
+//       },
+//     };
+//   }
 
-  async componentDidMount() {
-    const token = process.env.REACT_APP_TOKEN;
-    console.log("Env token:", token);
+//   async componentDidMount() {
+//     const token = process.env.REACT_APP_TOKEN;
+//     console.log("Env token:", token);
 
-    const data = await fetch("https://api.github.com/users/Monu2114", {
-      headers: {
-        Authorization: `token ${token}`,
-      },
-    });
-    const json_data = await data.json();
-    console.log(json_data);
+//     const data = await fetch("https://api.github.com/users/Monu2114", {
+//       headers: {
+//         Authorization: `token ${token}`,
+//       },
+//     });
+//     const json_data = await data.json();
+//     console.log(json_data);
 
-    this.setState({
-      userInfo: json_data,
-    });
-  }
-  render() {
-    console.log("child class component rendered");
+//     this.setState({
+//       userInfo: json_data,
+//     });
+//   }
+//   render() {
+//     console.log("child class component rendered");
 
-    const { name, location, avatar_url } = this.state.userInfo;
-    return (
-      <div className="user-card">
-        <img src={avatar_url}></img>
-        <h2>Name : {name}</h2>
-        <h3>Location : {location}</h3>
-        <h4>Phone Number : 1234567890</h4>
-      </div>
-    );
-  }
-}
+//     const { name, location, avatar_url } = this.state.userInfo;
+//     return (
+//       <div className="user-card">
+//         <img src={avatar_url}></img>
+//         <h2>Name : {name}</h2>
+//         <h3>Location : {location}</h3>
+//         <h4>Phone Number : 1234567890</h4>
+//       </div>
+//     );
+//   }
+// }
